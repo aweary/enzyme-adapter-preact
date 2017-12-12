d4555966b15b95f8ac67878f6423a2f03044018f
awinogradov <winogradovaa@gmail.com>
2017-12-12 15:38:45 +0300

    Improve tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     setupFiles : ['./__tests__/index.js'],
+    testMatch: ['**/__tests__/*.spec.js'],
     moduleNameMapper : {
         'preact-enzyme-adapter' : `<rootDir>/src/index.js`
     }
